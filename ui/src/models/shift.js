36c4a881c10c039019d6fc19f9ebf94e2daf27d2
root <root@LAPTOP-2KFUO51R.>
2025-04-06 09:24:07 +0000

    ui login

---
+++ ui/src/models/shift.js
@@ -0,0 +1,10 @@
+const Shift = function (id, name, start, end, owner, line) {
+    this.id = id;
+    this.name = name;
+    this.start = start;
+    this.end = end;
+    this.owner = owner;
+    this.line = line;
+};
+
+export default Shift;
